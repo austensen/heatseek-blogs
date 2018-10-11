99cf48e490a5932bebefcbb41cfc3261a6e31262
Maxwell Austensen <maxwell.austensen@nyu.edu>
2018-10-11 04:25:04 -0400

    add magick image manipulation helper functions for plot, and logo images

--- R/theme_heatseek.R
+++ R/theme_heatseek.R
@@ -18,3 +18,42 @@ theme_heatseek <- function(base_size = 11,
     plot.caption = ggplot2::element_text(face = "italic", color = "darkgrey", vjust = -0.2)
   )
 }
+
+# Returns Heat Seek logo as a {magick} image
+heatseek_logo <- function(scale_geometry = 500) {
+  img <- magick::image_read(here::here("img", "logos", "heatseek-logo.png"))
+  img <- magick::image_scale(img, geometry = scale_geometry)
+  img
+}
+  
+# Returns Mapbox logo as a {magick} image
+mapbox_logo <- function(scale_geometry = 200) {
+  img <- magick::image_read(here::here("img", "logos", "mapbox-logo.png"))
+  img <- magick::image_scale(img, geometry = scale_geometry)
+  img <- magick::image_quantize(img, colorspace = "gray")
+  img <- magick::image_colorize(img, opacity = 0.6, "grey")
+  img
+}
+# Returns Mapbox logo as a {magick} image
+osm_logo <- function(scale_geometry = 200) {
+  img <- magick::image_read(here::here("img", "logos", "osm-logo.png"))
+  img <- magick::image_scale(img, geometry = scale_geometry)
+  img <- magick::image_quantize(img, colorspace = "gray")
+  img <- magick::image_colorize(img, opacity = 0.6, "grey")
+  img
+}
+  
+# Works live ggsave() but returns a {magick} image
+ggimage <- function(plot = ggplot2::last_plot(), width, height, units = "in", device = "png", ...) {
+  plot_file <- tempfile()
+  ggsave(
+    filename = plot_file, 
+    plot = plot, 
+    width = width, 
+    height = height, 
+    units = units, 
+    device = device, 
+    ...
+  )
+  magick::image_read(plot_file)
+}
